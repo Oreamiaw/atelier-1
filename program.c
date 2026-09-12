32ac760039ec8bb9bf0d948161ac44b311570854
Oreamiaw <tristan.crombecque33@gmail.com>
2026-09-12 14:58:20 +0000

    V1 (version complète n°1)

--- program.c
+++ program.c
@@ -1,7 +1,8 @@
+//instrument de Jayson DUBOST et Tristan CROMBECQUE
 #include <stdio.h>
 
 int main() {
-
+//le décallage aux lignes 8 et 9 est du à l'utilisation du slash inversé
 
     printf("+----------------------------------------------------------+\n");
     printf("|                                                          |\n");
@@ -12,7 +13,7 @@ int main() {
     printf("|  |   |                                            |   |  |\n");
     printf("|  |   |                     ---                    |   |  |\n");
     printf("|  | - |                                            | - |  |\n");
-    printf("|  |   | __     ________            ________   ____ |   |  |\n");
+    printf("|  |   | __     ________             _______   ____ |   |  |\n");
     printf("|--|---|<85|-----------||-----+-----||--------|2500>|---|--|\n");
     printf("|  |   ||__|           ||           ||        |____||   |  |\n");
     printf("|  | - |kts                                       ft| - |  |\n");
@@ -21,8 +22,8 @@ int main() {
     printf("|  | - |                 5---------5                | - |  |\n");
     printf("|  |   |                                       29.92|   |  |\n");
     printf("|  +---+                                            +---+  |\n");
-    printf("|                                                          |\n");
-    printf("|                                                          |\n");
+    printf("|              _______________|_______________             |\n");
+    printf("|             |  -  |E|  -  |315|  -  |N|  -  |            |\n");
     printf("+----------------------------------------------------------+\n");
     
 
