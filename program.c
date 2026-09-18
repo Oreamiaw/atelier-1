29ee4f8ce7b2223a1c70fdf80623b2b14dabb2d6
Oreamiaw <tristan.crombecque33@gmail.com>
2026-09-18 09:46:09 +0000

    V5 LIEN QNH-ALTITUDE

--- program.c
+++ program.c
@@ -1,4 +1,7 @@
 //instrument de Jayson DUBOST et Tristan CROMBECQUE
+
+//définition des variables et appels
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,7 +9,10 @@
 int speed;//définir la vitesse à 0kts
 int alt; // définir l'altitude à 0ft
 int hdg; //définir le cap suivi
-float QNH = 30.92; // pression de référence (inHg)
+float QNH; // pression de référence (inHg)
+
+#define QNH_STD 29.92   // QNH standard de référence
+#define ALT_BASE 950    // altitude de base quand QNH = QNH_STD
 
 
 int main() {
@@ -16,7 +22,11 @@ int main() {
     speed = rand() % 100; // speed devient aléatoire entre 0 et 99kts
     alt = rand() % 10000; // alt devient aléatoire entre 0 et 9999ft
     hdg = rand() % 360; // hdg devient aléatoire entre 0 et 359
-    QNH = 25.0 + (rand() % 1001) / 100.0; // QNH aléatoire entre 25.00 et 35.00
+    QNH = 28.0 + (rand() % 1001) / 100.0; // QNH aléatoire entre 25.00 et 35.00
+
+    // pour chaque +0.01 de QNH par rapport à QNH_STD, alt augmente de 10ft
+    alt = ALT_BASE + (int)((QNH - QNH_STD) * 1000); 
+    // (QNH - QNH_STD) donne un écart en inHg, *1000 convertit chaque 0.01 en 10ft
 
     char gauche, droite; //utilisation du char pour les points cardinaux
 
