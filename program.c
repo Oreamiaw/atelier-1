c888cccf8749bd8832c39f1cac9e3f09796ea65e
Oreamiaw <tristan.crombecque33@gmail.com>
2026-09-18 09:23:38 +0000

    V3 ajout variable aléatoire sur la boussole (N,E,S,W) sont reliés

--- program.c
+++ program.c
@@ -1,14 +1,36 @@
 //instrument de Jayson DUBOST et Tristan CROMBECQUE
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int speed = 85; //définir la vitesse à 85kts
-int alt = 950; // définir l'altitude à 2500ft
-int hdg = 315; //définir le cap suivi
-
+int speed = 0; //définir la vitesse à 0kts
+int alt = 0; // définir l'altitude à 0ft
+int hdg = 0; //définir le cap suivi
 
 
 int main() {
-//le décallage aux lignes 8 et 9 est du à l'utilisation du slash inversé
+//valeures de la vitesse et de l'altitude prises aléatoirement
+
+    srand(time(NULL)); // initialisation du générateur aléatoire
+    speed = rand() % 100; // speed devient aléatoire entre 0 et 99kts
+    alt = rand() % 10000; // alt devient aléatoire entre 0 et 9999ft
+    hdg = rand() % 360; // hdg devient aléatoire entre 0 et 359
+
+    char gauche, droite;
+
+    if (hdg >= 0 && hdg < 90) {
+        gauche = 'N';
+        droite = 'E';
+    } else if (hdg >= 90 && hdg < 180) {
+        gauche = 'E';
+        droite = 'S';
+    } else if (hdg >= 180 && hdg < 270) {
+        gauche = 'S';
+        droite = 'W';
+    } else { // hdg entre 270 et 359
+        gauche = 'W';
+        droite = 'N';
+    }
 
     printf("+----------------------------------------------------------+\n");
     printf("|                                                          |\n");
@@ -29,7 +51,7 @@ int main() {
     printf("|  |   |                                       29.92|   |  |\n");
     printf("|  +---+                                            +---+  |\n");
     printf("|              _______________|_______________             |\n");
-    printf("|             |  -  |W|  -  |%3d|  -  |N|  -  |            |\n", hdg);
+    printf("|             |  -  |%c|  -  |%3d|  -  |%c|  -  |            |\n",gauche, hdg, droite);
     printf("+----------------------------------------------------------+\n");
     
 
