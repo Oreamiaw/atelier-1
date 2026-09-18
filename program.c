c0aa4e6c382bc5eb920a9fe39c0c4981a54f00d0
Oreamiaw <tristan.crombecque33@gmail.com>
2026-09-18 08:29:41 +0000

    V2 (modification nombres fixes en variables)

--- program.c
+++ program.c
@@ -1,6 +1,12 @@
 //instrument de Jayson DUBOST et Tristan CROMBECQUE
 #include <stdio.h>
 
+int speed = 85; //définir la vitesse à 85kts
+int alt = 950; // définir l'altitude à 2500ft
+int hdg = 315; //définir le cap suivi
+
+
+
 int main() {
 //le décallage aux lignes 8 et 9 est du à l'utilisation du slash inversé
 
@@ -14,7 +20,7 @@ int main() {
     printf("|  |   |                     ---                    |   |  |\n");
     printf("|  | - |                                            | - |  |\n");
     printf("|  |   | __     ________             _______   ____ |   |  |\n");
-    printf("|--|---|<85|-----------||-----+-----||--------|2500>|---|--|\n");
+    printf("|--|---|<%2d|-----------||-----+-----||--------|%4d>|---|--|\n", speed, alt); 
     printf("|  |   ||__|           ||           ||        |____||   |  |\n");
     printf("|  | - |kts                                       ft| - |  |\n");
     printf("|  |   |                     ---                    |   |  |\n");
@@ -23,7 +29,7 @@ int main() {
     printf("|  |   |                                       29.92|   |  |\n");
     printf("|  +---+                                            +---+  |\n");
     printf("|              _______________|_______________             |\n");
-    printf("|             |  -  |W|  -  |315|  -  |N|  -  |            |\n");
+    printf("|             |  -  |W|  -  |%3d|  -  |N|  -  |            |\n", hdg);
     printf("+----------------------------------------------------------+\n");
     
 
